6f997350d46a9c0d88f20ce994a73dd0fb170be9
Aluno ETEC HAS <marisol.poma15@hotmail.com>
2019-05-29 18:59:58 -0300

    Aula 24052019 Ds Projeto Mvc

---
+++ ProjetoMVC/ProjetoMVC/Context/ProdutoContext.cs
@@ -0,0 +1,30 @@
+﻿using ProjetoMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoMVC.Context
+{
+    public class ProdutoContext : DbContext
+    {
+        public ProdutoContext() : base ("ConexaoHoracio")
+        {
+
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        /* para mudar o nome do  banco */
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Conventions.Remove<PluralingTableNameConvertion>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ ProjetoMVC/ProjetoMVC/Models/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoMVC.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MiniLength(2,ErrorMessage = "Minimo de 2 caracteres")]
+        [MiniLegth(150,ErrorMessage = "Maximo de caracteres foi excebido")]
+        public  string Descricao{ get; set; }
+
+
+       
+        public decimal Valor { get; set; }
+
+
+        [Displayname("Data de Validade ")]
+        [Required(ErrorMessagem = "Digite a data de validade")]
+        public DateTime DataValidade { get; set; }
+
+        
+        public DateTime  DataCadastro { get; set; }
+
+
+
+
+
+
+
+    }
+}
